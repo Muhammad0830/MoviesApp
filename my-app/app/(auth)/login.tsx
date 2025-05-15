d0302209d711357e43d285d82379535d0a1985fd
Muhammad0830 <abduqayumovm1@gmail.com>
2025-05-15 18:48:35 +0500

    login / sign up pages design enhanced

--- my-app/app/(auth)/login.tsx
+++ my-app/app/(auth)/login.tsx
@@ -1,10 +1,12 @@
-import { View, TextInput, Button, Alert, Text } from "react-native";
+import { View, TextInput, Button, Alert, Text, Image, TouchableOpacity } from "react-native";
 import { useEffect, useState } from "react";
 import { useNavigation, useRouter } from "expo-router";
 import * as SecureStore from "expo-secure-store";
 import { URL_CONFIG } from "@/services/api";
 import { useAuth } from "@/contexts/authContext";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -44,7 +46,7 @@ export default function Login() {
         return;
       }
       await AsyncStorage.setItem("token", token);
-      console.log('token from storage', AsyncStorage.getItem("token"))
+      console.log("token from storage", AsyncStorage.getItem("token"));
       setUser({ id: decoded.id, email: decoded.email });
       router.replace("(tabs)" as any);
       console.log("setUser", { id: decoded.id, email: decoded.email });
@@ -76,23 +78,43 @@ export default function Login() {
   };
 
   return (
-    <View style={{ padding: 20 }}>
-      <TextInput placeholder="Email" onChangeText={setEmail} />
-      <TextInput
-        placeholder="Password"
-        secureTextEntry
-        onChangeText={setPassword}
+    <View className="bg-bg_primary flex-1 px-4">
+      <Image
+        source={images.bg}
+        className="absolute left-0 right-0 top-0 bottom-0 z-0"
       />
-      <Button title="Login" onPress={handleLogin} />
-      <Text>
-        you don't have an account?{" "}
-        <Text
-          className="text-blue-500 text-[14px] italic"
-          onPress={() => router.replace("(auth)/signup" as any)}
-        >
-          signup
-        </Text>
-      </Text>
+      <Image source={icons.logo} className="w-12 h-10 mt-10 mb-5 mx-auto" />
+      <View className="flex-1 justify-center">
+        <View className="border border-white rounded-lg px-2">
+          <TextInput
+            placeholder="Email"
+            onChangeText={setEmail}
+            className="text-white"
+            placeholderTextColor={"gray"}
+          />
+        </View>
+        <View className="border border-white rounded-lg px-2 my-2">
+          <TextInput
+            placeholder="Password"
+            secureTextEntry
+            onChangeText={setPassword}
+            className="text-white"
+            placeholderTextColor={"gray"}
+          />
+        </View>
+        <TouchableOpacity className="bg-blue-500 justify-center items-center rounded-lg px-2 py-2" onPress={handleLogin}>
+          <Text className="text-white text-[20px] font-bold">Login</Text>
+        </TouchableOpacity>
+        <View className="mt-1 flex-row gap-2 items-end">
+          <Text className="text-white text-[14px]">You don't have an account?</Text>
+          <Text
+            className="text-blue-500 text-[16px] italic"
+            onPress={() => router.replace("(auth)/signup" as any)}
+          >
+            Signup
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
--- my-app/app/(auth)/signup.tsx
+++ my-app/app/(auth)/signup.tsx
@@ -1,9 +1,11 @@
-import { View, Text, Button, Alert } from "react-native";
+import { View, Text, Button, Alert, TouchableOpacity, Image } from "react-native";
 import React, { useState } from "react";
 import { TextInput } from "react-native-gesture-handler";
 import * as SecureStore from 'expo-secure-store';
 import { useRouter } from "expo-router";
 import { URL_CONFIG } from "@/services/api";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
 const signup = () => {
   const [username, setName] = useState("");
@@ -34,24 +36,56 @@ const signup = () => {
   };
 
   return (
-    <View style={{ padding: 20 }}>
-      <TextInput placeholder="Username" onChangeText={setName} />
-      <TextInput placeholder="Email" onChangeText={setEmail} />
-      <TextInput
-        placeholder="Password"
-        secureTextEntry
-        onChangeText={setPassword}
+    <View className="bg-bg_primary flex-1 px-4">
+      <Image
+        source={images.bg}
+        className="absolute left-0 right-0 top-0 bottom-0 z-0"
       />
-      <Button title="Sign Up" onPress={handleSignUp} />
-      <Text>
-        you don't have an account?{" "}
-        <Text
-          className="text-blue-500 text-[14px] italic"
-          onPress={() => router.replace("(auth)/login" as any)}
+      <Image source={icons.logo} className="w-12 h-10 mt-10 mb-5 mx-auto" />
+      <View className="flex-1 justify-center">
+        <View className="border border-white rounded-lg px-2 mb-2">
+          <TextInput
+            placeholder="Username"
+            onChangeText={setName}
+            className="text-white"
+            placeholderTextColor={"gray"}
+          />
+        </View>
+        <View className="border border-white rounded-lg px-2">
+          <TextInput
+            placeholder="Email"
+            onChangeText={setEmail}
+            className="text-white"
+            placeholderTextColor={"gray"}
+          />
+        </View>
+        <View className="border border-white rounded-lg px-2 my-2">
+          <TextInput
+            placeholder="Password"
+            secureTextEntry
+            onChangeText={setPassword}
+            className="text-white"
+            placeholderTextColor={"gray"}
+          />
+        </View>
+        <TouchableOpacity
+          className="bg-blue-500 justify-center items-center rounded-lg px-2 py-2"
+          onPress={handleSignUp}
         >
-          login
-        </Text>
-      </Text>
+          <Text className="text-white text-[20px] font-bold">Sign Up</Text>
+        </TouchableOpacity>
+        <View className="mt-1 flex-row gap-2 items-end">
+          <Text className="text-white text-[14px]">
+            Already have an account?
+          </Text>
+          <Text
+            className="text-blue-500 text-[16px] italic"
+            onPress={() => router.replace("(auth)/login" as any)}
+          >
+            Login
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
