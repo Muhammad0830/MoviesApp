8bc7984f7519451cff56f81c32ab1b5740c48f2e
Muhammad0830 <abduqayumovm1@gmail.com>
2025-05-27 16:28:07 +0500

    delete from saved movies ui enhanced
    ###delete from saved movies ui enhanced
    
    ### score bug fixed
    
    ### rate icon changes when rated without refreshing the page

--- my-app/components/horizontalMovieCard.tsx
+++ my-app/components/horizontalMovieCard.tsx
@@ -3,18 +3,24 @@ import React from "react";
 import { Link } from "expo-router";
 import { icons } from "@/constants/icons";
 
+const formatNumber = (value: number): string => {
+  let number = Number(value);
+
+  return number.toString();
+};
+
 const HorizontalMovieCard = ({ item, gap, index }: any) => {
   return (
     <Link href={`/movies/${item.id}` as any} asChild>
-      <TouchableOpacity
-        className='w-[150px] h-full'
-      >
+      <TouchableOpacity className="w-[150px] h-full">
         <View
           className={`rounded-md h-full border border-primary/40 flex flex-col overflow-hidden`}
           style={{ marginHorizontal: gap }}
         >
           <View className="absolute w-7 h-7 top-1 left-1 z-10 rounded-full bg-primaryDarker border border-black/70 items-center justify-center">
-            <Text className="text-white text-[14px] font-bold">{index + 1}</Text>
+            <Text className="text-white text-[14px] font-bold">
+              {index + 1}
+            </Text>
           </View>
           <Image
             source={{ uri: item.movie_banner }}
@@ -30,7 +36,9 @@ const HorizontalMovieCard = ({ item, gap, index }: any) => {
             <View className="flex flex-row justify-between items-center">
               <View className="flex flex-row gap-1 items-center">
                 <Image source={icons.star} className="w-2 h-2" />
-                <Text className="text-white text-[8px]">{item.score}</Text>
+                <Text className="text-white text-[8px]">
+                  {formatNumber(item.score)}
+                </Text>
               </View>
               <Text className="text-white text-[8px] font-light">
                 {item.release_date}
