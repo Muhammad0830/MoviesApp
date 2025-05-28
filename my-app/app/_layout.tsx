70e7c072be54827acc46714ae555601e3be307c8
Muhammad0830 <abduqayumovm1@gmail.com>
2025-05-28 09:24:43 +0500

    StatusBar added

--- my-app/app/_layout.tsx
+++ my-app/app/_layout.tsx
@@ -4,11 +4,16 @@ import "./globals.css";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-reanimated";
 import { AuthProvider } from "@/contexts/authContext";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <AuthProvider>
+        <StatusBar
+          barStyle="dark-content" // options: 'default', 'light-content', 'dark-content'
+          backgroundColor="#b297f0" // Android only
+        />
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="(auth)" />
           <Stack.Screen name="(tabs)" />
